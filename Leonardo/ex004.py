e5bcac43252bbce4cf07502274798fd036e7bbaa
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2022-04-08 10:34:05 -0300

    08/04

---
+++ Leonardo/ex004.py
@@ -0,0 +1,12 @@
+idade = int(input('Digite sua idade: '))
+
+if 5 <= idade <= 10:
+    print('Infantil')
+elif 11 <= idade <= 15:
+    print('Juvenil')
+elif 16 <= idade <= 20:
+    print('Júnior')
+elif 21<= idade <= 25:
+    print('Profissional')
+else:
+    print('idade invalida')
